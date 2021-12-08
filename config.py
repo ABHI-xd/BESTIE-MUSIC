b1c84ecfc4dcd31a8ff4eeda55f8397e3a1efb41
ABHI-xd <94308307+ABHI-xd@users.noreply.github.com>
2021-12-08 21:51:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ class Configs:
     API_ID = int(getenv("API_ID", "0"))
     API_HASH = getenv("API_HASH", "abc123")
     BOT_TOKEN = getenv("BOT_TOKEN", "123:abc")
-    OWNER_ID = int(getenv("OWNER_ID", ""))
+    OWNER_ID = int(getenv("OWNER_ID", "2117545440"))
     SESSION = getenv("SESSION", "session")
     CHANNEL_LINK = getenv("CHANNEL_LINK", "https://t.me/snehabhi_updates")
     GROUP_LINK = getenv("GROUP_LINK", "https://t.me/snehabhi_server")
